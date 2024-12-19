5ade23aed757958400d8f82f8ce9ddb080214859
Mathiyalagan-22 <mathiyalaganmarimuthu1@gmail.com>
2024-12-19 17:25:05 +0530

    ain

---
+++ projct-1/queryselector.js
@@ -0,0 +1,5 @@
+const heading = document.querySelectorAll('h1')
+
+for(let i=0; i<heading.length ; i++){
+    heading[i].style.color = 'red'
+}
---
+++ projct-1/script2.js
@@ -0,0 +1,19 @@
+// const wrapper =document.querySelector('.wrapper');
+// const LoginLink =document.querySelector('.Login-link');
+// const registerLink =document.querySelector('.register-link');
+// const btnPop =document.querySelector('.btn-pop');
+// registerLink.addEventListener('Click', ()=> {
+//     wrapper.classList.add('active');
+// });
+
+// LoginLink.addEventListener('Click', ()=> {
+//     wrapper.classList.remove('active');
+// });
+
+// btnPop.addEventListener('Click', ()=> {
+//     wrapper.classList.add('active-pop');
+// });
+
+// btnPop.addEventListener('Click', ()=> {
+//     wrapper.classList.remove('active-pop');
+// });
